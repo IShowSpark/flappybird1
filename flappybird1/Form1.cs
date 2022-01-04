2af4b1381a91b1e5cb961574f5137cb530218b36
ArtjomRozhkov <93324182+ArtjomRozhkov@users.noreply.github.com>
2022-01-04 14:07:00 +0200

    Update Form1.cs

--- flappybird1/Form1.cs
+++ flappybird1/Form1.cs
@@ -97,3 +97,78 @@ namespace flappybird1
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-----------------------------------------------------
+using System;
+
+namespace EpicBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] hero = { "Bambalbi", "Spider man", "Batman", "Doo-Skooby" };
+            string[] enemy = { "Adolf", "Armagedon", "Broodmother", "Meepo", "Fucking slave", "Gachi Muchi" };
+            string[] weapon = { "awp", "knife", "glock", "m4a1", "desert eagle", "Carbine Rifle", "Assault Rifle" };
+
+            string randomhero = GetRandomCharacter(hero);
+            string randomenemy = GetRandomCharacter(enemy);
+            string randomweapon_hero = GetWeapon(weapon);
+            string randomweapon_enemy = GetWeapon(weapon);
+
+            Console.WriteLine($"Your random hero is {randomhero} and his weapon is {randomweapon_hero}");
+            Console.WriteLine($"Your random enemy is {randomenemy} and his weapon is {randomweapon_enemy}");
+
+        }
+        public static string GetRandomCharacter(string[] someArray)
+        {
+            return someArray[GetRandomIndexForArray(someArray)];
+        } 
+        public static string GetWeapon(string[] weapon)
+        {
+            Random rnd = new Random();
+            int random_Index = rnd.Next(0, weapon.Length);
+
+            return weapon[random_Index];
+        }
+        public static int GetRandomIndexForArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, someArray.Length)
+        }
+    }
+}
